dd0f4928623adcefc0705ffd32a0a2126d28b7ef
gabriela.ttavares <gabriela.ttavares@gmail.com>
2022-03-24 14:32:36 -0300

    feat: adiciona a rota p buscar um produto por id

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('produtos')
 export class ProdutosController {
   @Get()
   obterTodos(): string {
     return 'Lista de todos os produtos';
+  }
+
+  @Get(':id')
+  obterUm(@Param() params): string {
+      return `Retorna os dados do produto ${params.id}`;
   }
 }
